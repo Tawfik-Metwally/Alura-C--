7810e324b4fa895b4aeb15bed1c4dce22116d68a
Tawfik Metwally <60214510+Tawfik-Metwally@users.noreply.github.com>
2023-02-27 11:46:57 -0300

    Verificando chutes

--- Conhecendo a linguagem e a STL/Jogo da Forca/Jogodaforca.cpp
+++ Conhecendo a linguagem e a STL/Jogo da Forca/Jogodaforca.cpp
@@ -2,9 +2,20 @@
 #include <string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+bool letra_existe(char chute) {
+	for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
+	{
+		if (chute == PALAVRA_SECRETA[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+
 int main() {
-	string palavra_secreta = "MELANCIA";
-	cout << palavra_secreta << endl;
+	cout << PALAVRA_SECRETA << endl;
 
 	bool nao_acertou = true;
 	bool nao_enforcou = true;
@@ -14,5 +25,13 @@ int main() {
 		cin >> chute;
 
 		cout << "Seu chute foi " << chute << endl;
+
+		if (letra_existe(chute))
+		{
+			cout << "Parabens! Seu chute esta certo!" << endl;
+		}
+		else {
+			cout << "Voce errou! Tente novamente!" << endl;
+		}
 	}
 }
