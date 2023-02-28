2c57618c8e94e70d8438aa698fa134e0dafac88c
Metwally, Tawfik <Tawfik.Metwally@dieboldnixdorf.com>
2023-02-28 10:21:11 -0300

    Mostrando chutes errados

--- Conhecendo a linguagem e a STL/Jogo da Forca/Jogodaforca.cpp
+++ Conhecendo a linguagem e a STL/Jogo da Forca/Jogodaforca.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <map>
+#include<vector>
 #include <string>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map <char, bool> chutou;
+vector <char> chutes_errados;
 
 bool letra_existe(char chute) {
 	/*for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
@@ -15,24 +19,43 @@ bool letra_existe(char chute) {
 	//Forma nova do C++11 (g++ Jogodaforca.cpp -o Jogodaforca.exe -std=c++11)
 	//Com makefile (mingw32-make Jogodaforca)
 	for (char letra : PALAVRA_SECRETA) {
-		if (chute == letra) {
+		if (chute == letra)
 			return true;
-		}
 	}
 	return false;
 }
 
 int main() {
-	cout << PALAVRA_SECRETA << endl;
+	//cout << PALAVRA_SECRETA << endl;
 
 	bool nao_acertou = true;
 	bool nao_enforcou = true;
 
+	cout << "*********************" << endl;
+	cout << "*** Jogo da Forca ***" << endl;
+	cout << "*********************" << endl;
 	while (nao_acertou && nao_enforcou) {
+
+		cout << "Chutes errados: ";
+		for (char letra : chutes_errados)
+		{
+			cout << letra << " ";
+		}
+		cout << endl;
+		for (char letra : PALAVRA_SECRETA)
+		{
+			if (chutou[letra])
+				cout << letra << " ";
+			else
+				cout << "_ ";
+		}
+		cout << endl;
 		char chute;
 		cin >> chute;
 
-		cout << "Seu chute foi " << chute << endl;
+		chutou[chute] = true;
+
+		//cout << "Seu chute foi " << chute << endl;
 
 		if (letra_existe(chute))
 		{
@@ -40,6 +63,8 @@ int main() {
 		}
 		else {
 			cout << "Voce errou! Tente novamente!" << endl;
+			chutes_errados.push_back(chute);
 		}
+		cout << endl;
 	}
 }
