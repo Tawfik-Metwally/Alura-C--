0f4b9cf8e1274674d8e2f9105f8a843de92faa8d
Metwally, Tawfik <tawfikaly02@gmail.com>
2023-03-13 10:52:23 -0300

    Definindo funções de saque e deposito

--- Orientação a Objetos com C++ Classes, métodos e atributos/Banco/Banco.cpp
+++ Orientação a Objetos com C++ Classes, métodos e atributos/Banco/Banco.cpp
@@ -10,6 +10,26 @@ struct Conta {
 	float saldo;
 };
 
+void sacar(Conta& conta, float valoASacar) {
+	if (valoASacar < 0) {
+		cout << "Nao pode sacar valor negativo!" << endl;
+		return;
+	}
+	if (valoASacar > conta.saldo) {
+		cout << "Saldo insuficiente!" << endl;
+		return;
+	}
+	conta.saldo -= valoASacar;
+}
+
+void depositar(Conta& conta, float valoADepositar) {
+	if (valoADepositar < 0) {
+		cout << "Nao pode sacar valor negativo!" << endl;
+		return;
+	}
+	conta.saldo += valoADepositar;
+}
+
 int main() {
 	Conta UmaConta;
 	UmaConta.numero = "123456";
@@ -19,6 +39,8 @@ int main() {
 
 	Conta OutraConta;
 	OutraConta.saldo = 200;
+
+	depositar(UmaConta, 300);
 
 	cout << "UmaConta: " << UmaConta.saldo << " OutraConta: " << OutraConta.saldo << endl;
 }
